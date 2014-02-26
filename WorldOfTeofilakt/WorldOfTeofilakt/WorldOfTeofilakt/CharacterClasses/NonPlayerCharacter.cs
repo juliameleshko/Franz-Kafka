3400ae42d1602ea8e29ae1752d82ee5f76a87d45
dob-sal <dob_sal@yahoo.com>
2014-02-26 21:52:42 +0200

    NonPlayerCharacter.cs

---
+++ WorldOfTeofilakt/WorldOfTeofilakt/WorldOfTeofilakt/CharacterClasses/NonPlayerCharacter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+
+namespace WorldOfTeofilakt.CharacterClasses
+{
+    class NonPlayerCharacter : Character
+    {
+        private NonPlayerCharacterTypes type;
+        
+        public NonPlayerCharacter(string name, Texture2D image, Vector2 position, NonPlayerCharacterTypes type)
+            :base(name, image, position)
+        {
+            this.type = type;
+        }
+
+    }
+}
